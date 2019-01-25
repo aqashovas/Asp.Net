ff5fd84a0e77c63f9fd098390db66f6e90431e97
Sabina Aqashova <aqashova@inbox.ru>
2019-01-25 22:38:35 +0400

    First

---
+++ AspExample1/Controllers/ForgotPasswordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspExample1.Controllers
+{
+    public class ForgotPasswordController : Controller
+    {
+        // GET: ForgotPassword
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspExample1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspExample1.MvcApplication" Language="C#" %>
